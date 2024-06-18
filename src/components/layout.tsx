370a9fc8e2b092317e7572aa3cd4622068d5dde9
DannyGJdeJong <dannygjdejong@gmail.com>
2024-06-18 15:30:00 +0200

    Fix background not showing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { GlobalStyles } from "@mui/material";
 
-import Background from "../assets/background.svg";
+import Background from "../assets/background.svg?url";
 
 export const Layout = ({ children }: React.PropsWithChildren) => {
   return (
@@ -9,7 +9,7 @@ export const Layout = ({ children }: React.PropsWithChildren) => {
       <GlobalStyles
         styles={{
           body: {
-            background: `url(${Background}) no-repeat center center fixed`,
+            background: `url("${Background}") no-repeat center center fixed`,
             backgroundSize: "cover",
             height: "100%",
             overflow: "hidden",
