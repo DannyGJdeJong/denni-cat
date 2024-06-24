cb16a3b9f4f27b1cec24ac0dc8f2fb73d9fdc073
DannyGJdeJong <dannygjdejong@gmail.com>
2024-06-24 15:28:19 +0200

    Update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
-export const API = import.meta.env.VITE_BACKEND_API || "http://localhost:5000/";
+export const BARQ_URL = "https://barq.denni.cat";
+export const TELEGRAM_URL = "https://tg.denni.cat";
+export const DISCORD_URL = "https://dc.denni.cat";
+export const INSTAGRAM_URL = "https://ig.denni.cat";
+export const TWITTER_URL = "https://tw.denni.cat";
+export const MASTODON_URL = "https://fedi.denni.cat";
+export const BLUESKY_URL = "https://bsky.denni.cat";
+export const TIKTOK_URL = "https://tt.denni.cat";
 
-export const BARQ_URL = import.meta.env.VITE_BARQ_URL || "";
-export const TELEGRAM_URL = import.meta.env.VITE_TELEGRAM_URL || "";
-export const DISCORD_URL = import.meta.env.VITE_DISCORD_URL || "";
-export const TWITTER_URL = import.meta.env.VITE_TWITTER_URL || "";
-export const MASTODON_URL = import.meta.env.VITE_MASTODON_URL || "";
-export const BLUESKY_URL = import.meta.env.VITE_BLUESKY_URL || "";
-export const TIKTOK_URL = import.meta.env.VITE_TIKTOK_URL || "";
-export const API_URL = import.meta.env.VITE_API_URL || "";
+export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/";
